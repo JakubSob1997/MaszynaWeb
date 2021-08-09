05d444c2a97942f38f69b66f8fd0abb7c651a625
JakubSob1997 <jakubsob97@gmail.com>
2021-08-09 12:38:34 +0200

    added console styling

--- scripts/console-device.js
+++ scripts/console-device.js
@@ -1,5 +1,7 @@
 
 
+import Alerter from "./alerter.js";
+import { AlertStyleEnum } from "./enums.js";
 import IODevice from "./io-device.js";
 
 export default class ConsoleDevice{
@@ -9,6 +11,44 @@ export default class ConsoleDevice{
         this.asciiInputIO = new IOConsoleASCIInput(this);
         this.consoleOutputIO = new IOConsoleOutput(this);
         this.clearConsoleIO=new IOConsoleClear(this);
+
+
+        this.asciiInputString ="";
+        this.numberInputVal =0;
+        this.consoleOutputString="";
+
+        this.onUpdateOutput=function(_device){(console.log("Overide Me"))};
+        this.onUpdateASCII=function(_device){(console.log("Overide Me"))};
+       
+    }
+
+
+    readASCII(){
+        if(this.asciiInputString===""){
+            Alerter.sendMessage("Add console wait behaviour",AlertStyleEnum.UnhandledException)
+            return 0;
+        }
+
+
+        const char =this.asciiInputString.charAt(0);
+        this.asciiInputString=this.asciiInputString.substring(1);
+        this.onUpdateASCII();
+        return char;
+    }
+
+    readNum(){
+        return this.numberInputVal;
+    }
+
+    writConsole(_value){
+        let char = String.fromCharCode(_value);
+        this.consoleOutputString+=char;
+        this.onUpdateOutput(this);
+    }
+
+    clearConsole(){
+        this.consoleOutputString="";
+        this.onUpdateOutput(this);
     }
 
 
@@ -26,7 +66,8 @@ class IOConsoleNumericInput extends IODevice{
             console.log("No io driver provided");
             return;
         }
-        console("(overide) interacted by io diriver "+ _IODriver)
+        const _val = _ConsoleDevice.readNum();
+        _IODriver.read(_val)
         _IODriver.confirm();
     }
 
@@ -46,7 +87,8 @@ class IOConsoleASCIInput extends IODevice{
             console.log("No io driver provided");
             return;
         }
-        console("(overide) interacted by io diriver "+ _IODriver)
+        const _val = _ConsoleDevice.readASCII();
+        _IODriver.read(_val)
         _IODriver.confirm();
     }
 
@@ -67,7 +109,8 @@ class IOConsoleOutput extends IODevice{
             console.log("No io driver provided");
             return;
         }
-        console("(overide) interacted by io diriver "+ _IODriver)
+        const _outVal = _IODriver.write()
+        _ConsoleDevice.writConsole(_outVal);
         _IODriver.confirm();
     }
 
@@ -88,7 +131,7 @@ class IOConsoleClear extends IODevice{
             console.log("No io driver provided");
             return;
         }
-        console("(overide) interacted by io diriver "+ _IODriver)
+        _ConsoleDevice.clearConsole();
         _IODriver.confirm();
     }
 
--- scripts/console-view.js
+++ scripts/console-view.js
@@ -18,7 +18,8 @@ export default class ConsoleView{
         this.wrapper = document.createElement("div");
         this.asciiInputEle = document.createElement("input");
         this.numericInputEle=document.createElement("input");
-        this.outputEle=document.createElement("div");
+        this.outputWrapper=document.createElement("div");
+        this.outputEle=document.createElement("textarea");
         this.clrConsoleButton=document.createElement("button");
 
 
@@ -30,18 +31,28 @@ export default class ConsoleView{
 
         this.wrapper.classList.add("generic-inspector");
         this.clrConsoleButton.classList.add("custom-btn");
+        this.outputWrapper.classList.add("console-output-wrapper");
+        this.outputEle.classList.add("console-output");
+        this.numericInputEle.setAttribute("value","0");
+        this.outputEle.setAttribute("readonly","true");
 
 
-        
+        this.outputEle.value="Lorem Ipsum is simp\nly dummy text of the printing and\n typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, re\nmaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 
+
+        this.outputWrapper.appendChild(this.outputEle);
 
         this.wrapper.appendChild(this.asciiInputEle);
         this.wrapper.appendChild(this.numericInputEle);
-        this.wrapper.appendChild(this.outputEle);
+        this.wrapper.appendChild(this.outputWrapper);
         this.wrapper.appendChild(this.clrConsoleButton);
 
-
         
+
+    }
+
+    addCalbacks(){
+
     }
 
 
